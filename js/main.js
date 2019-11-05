ee32cfc6ba50fb41472b8cc6c2c0fdfe06067db2
taruna <tarunapatel1998@gmail.com>
2019-11-05 14:56:00 +0530

    selection and deselection of element implemented

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 onload = function () {
     var s = Snap("#svg");
-    
+    let nodes = [];
 
     // var entity1 = createEntity(s,"1st")
 
@@ -18,48 +18,63 @@ onload = function () {
         console.log('finished dragging');
     }
 
-   
+    var clickCallback = function(event) {
+        let shp = this.select("rect");
+        if (shp == null){
+            shp = this.select("circle");
+        }
+        shp.attr({ 
+            strokeWidth: 5,
+         });
+        delselectAll(this.id);
+    };   
 
     // Create Entity Diagram
-    function createEntity(label) {
-        let entity = s.rect(100, 100, 100, 100),
-            entitylable = s.text(100+20,100+50)
+    function createEntity() {
+        let shp = s.rect(100, 100, 100, 100),
+            lbl = s.text(100+20,100+50)
         
-        entity.attr({
-            fill: "#bada55",
+        shp.attr({
+            fill: "#f5f2d0",
             stroke: "#000",
-            strokeWidth: 5,
+            strokeWidth: 1,
         });
-        entitylable.attr({
+        lbl.attr({
             'font-size':20,
             text:""
         });
 
-        g = s.g(entity,entitylable)
+        g = s.g(shp,lbl)
+
 
         g.dblclick(()=>{
             console.log("dbl")
             let doc = prompt("Type Desired label", 
                 "Entity");
                 if (doc != null){
-                    entitylable.attr({
+                    lbl.attr({
                         text:doc,
                     });
-                }
-            
+                }       
         });
+
+        g.click(clickCallback);
+        
         return g
         
     }
+
+
+
     // Create Process Shape
-    function createProcess(lable){
+    function createProcess(){
         let process = s.circle(100, 100, 80, 80),
             processlable = s.text(100-30,100+10)
         
         process.attr({
-            fill: "#bada55",
+            fill: "#f5f2d0",
             stroke: "#000",
-            strokeWidth: 5,
+            strokeWidth: 1,
         });
         processlable.attr({
             'font-size':20,
@@ -77,17 +92,18 @@ onload = function () {
                     });
                 }
                 });
+        g.click(clickCallback);
         return g
                 
         }
     // Create Data Store
-    function createDataStore(lable){
+    function createDataStore(){
         let shp = s.rect(100, 100,180, 60),
             lb = s.text(100+20,100+40)
         
         shp.attr({
-            fill: "#bada55",
-            strokeWidth: 5,
+            fill: "#f5f2d0",
+            strokeWidth: 1,
             stroke:"#000",
             strokeDasharray: "180 60 240",
         });
@@ -107,90 +123,138 @@ onload = function () {
                     });
                 }
                 });
-    
+                g.click(clickCallback)
         return g
 
     }
 
-    function createArrow(lable){
-        let rhl = s.circle(100-20,100,10,10)
-        let shp = s.line(100+10, 100,300, 100)
-        var Triangle = s.polyline("100,100 120,120 120,80");
-        let lntxt= s.text(100+60,100-10)
-        Triangle.attr({
-          fill: "#000",
+    function createArrow(){
+
+        
+        // let arw = s.polyline("100,100 120,120 120,80 102,100 300,100 ");
+
+        let body = s.line(100+10, 100,300, 100)
+        let head = s.polyline("100,100 120,120 120,80");
+        let label= s.text(100+60,100-10)
+        arw = s.g(body,head,label)
+        let lcrl = s.circle(100,100,5,5),
+            rcrl = s.circle(300,100,5,5)
+        
+
+        
+
+        head.attr({
+        //   fill: "#000",
+          stroke:"#000",
+          strokeWidth:4
           
         });  
-
-        rhl.attr({
-            visibility:"hidden",
-        });
         
-        shp.attr({
+        body.attr({
             strokeWidth: 5,
             stroke:"#000",
         });
-        lntxt.attr({
+        label.attr({
             'font-size':20,
             text:""
         });
-        g =  s.g(shp,Triangle,rhl,lntxt);
-        g.click(function () {
-           let e =  g.select("circle")
-           console.log(e)
-           e.attr({
-                
-                visibility:"visible",
-            })
-         });        
-         g.dblclick(()=>{
-            console.log("dbl")
-            let doc = prompt("Type Desired label", 
-                "arrow");
-                if (doc != null){
-                    lntxt.attr({
-                        text:doc,
-                    });
-                }
-                });
+
+        
+
+        
+        mv = function (dx, dy) {
+            this.attr({
+                transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
+            });
+            lcrl.attr({
+                transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
+            });
+            rcrl.attr({
+                transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
+            });
+        }
+
+        
+        arw.drag(mv, start, stop);
+        
+
+
+        rcrl.drag(function(dx,dy){
+            console.log("draging")
+            rcrl.attr({cx:+x+dx,cy:+y+dy});
+            body.attr({x2:+x+dx,y2:+y+dy});
+        },function(){
+            console.log("started")
+            x = body.attr("x2");
+            y = body.attr("y2");
+        },function(){});
+
+        lcrl.drag(function(dx,dy){
+            lcrl.attr({cx:+x+dx,cy:+y+dy});
+            arw.attr({x:+x+dx,x:+y+dy});
+        },function(){
+            
+            console.log("started")
+            x = body.attr("x1");
+            y = body.attr("y1");
+        },function(){});
+
+        // g =  s.g(shp,lntxt);       
+        //  g.dblclick(()=>{
+        //     console.log("dbl")
+        //     let doc = prompt("Type Desired label", 
+        //         "arrow");
+        //         if (doc != null){
+        //             lntxt.attr({
+        //                 text:doc,
+        //             });
+        //         }
+        //         });
     
-        return g
+        return null
 
     }
 
 
-    
+    function delselectAll(id){
+
+        nodes.forEach(node => {
+            if (node.id != id){
+                console.log(node.id)
+                let shp = node.select("rect");
+                if (shp == null){
+                    shp = node.select("circle");
+                }
+                shp.attr({
+                    strokeWidth: 1,
+                });   
+            }
+        });
+    }
+
     $("#addEntityBtn").click(function() {
-        let entity1 = createEntity("1st")
-        entity1.drag(move, start, stop);
+        let e = createEntity()
+        e.drag(move, start, stop);
+        nodes.push(e);
     });
 
     $("#addProcessBtn").click(function() {
-        let proces1 = createProcess("1st")
-        proces1.drag(move, start, stop);
+        let p = createProcess()
+        p.drag(move, start, stop);
+        nodes.push(p);
     });
 
     $("#addDataStoreBtn").click(function() {
-        let dataStore1 = createDataStore("1st")
-        dataStore1.drag(move, start, stop);
-        
+        let ds = createDataStore()
+        ds.drag(move, start, stop);
+        nodes.push(ds);
     });
 
     $("#addArrowBtn").click(function() {
-        let arrow1 =createArrow("1st")
-        arrow1.drag(move, start, stop);
-        
+        let arrow1 =createArrow()
+        // arrow1.drag(move, start, stop);
         
     });
 
 }
 
-// class Shape{
-//     constructor(){
-
-//     }
-// }
-
-// Shape.prototype.move = function(){
-//     this.attr
-// }
